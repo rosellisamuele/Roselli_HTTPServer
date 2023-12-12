3dbb34285125c1b6143fd882bbec793ae78aeef2
Samuele Roselli <samurose2005@gmail.com>
2023-12-12 17:20:53 +0100

    Updated HttpServer with only ServerSocket class

--- httpserver/src/main/java/httpserver/Server.java
+++ httpserver/src/main/java/httpserver/Server.java
@@ -1,14 +1,70 @@
 package httpserver;
 
-import com.sun.net.httpserver.HttpServer;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.InetSocketAddress;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 
 public class Server{
 
-    public static int port = 6789;
+    public static void main(String[] args) {
 
+        int port = 2222;
+
+        try{
+            ServerSocket serverSocket = new ServerSocket(port);
+            System.out.println("Server in ascolto sulla porta "+port);
+
+            while(true){
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Connessione accettata da "+clientSocket.getInetAddress());
+
+                handleClientRequest(clientSocket);
+
+                clientSocket.close();
+                System.out.println("Connessione chiusa");
+            }
+
+            
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+        
+    }
+
+    private static void handleClientRequest(Socket clientSocket){
+        try{
+            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            OutputStream outputStream = clientSocket.getOutputStream();
+
+            String requestLine = reader.readLine();
+            System.out.println("Richiesta ricevuta: "+requestLine);
+
+            String response = "HTTP/1.1 200 OK\r\n\r\nHello, world!";
+            outputStream.write(response.getBytes());
+
+            outputStream.flush();
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+    /* 
     public static void main(String[] args) throws IOException {     
         HttpServer server = create();
         start(server); 
@@ -27,5 +83,5 @@ public class Server{
         server.setExecutor(null);
         server.start();
     }
-
+*/
 }
